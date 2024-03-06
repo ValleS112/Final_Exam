9f5a279b50bbc256db4187be7949ae04151e47e6
lukl1 <156343801+lukl1@users.noreply.github.com>
2024-03-06 15:16:56 +0100

    added test

---
+++ task.py
@@ -0,0 +1,12 @@
+a11 = float(input("a11")) ; 
+a12 = float(input("a12")) ;
+a13 = float(input("a13")) ;
+a21 = float(input("a21")) ;
+a22 = float(input("a22")) ;
+a23 = float(input("a23")) ;
+a31 = float(input("a31")) ;
+a32 = float(input("a32")) ;
+a33 = float(input("a33")) ;
+
+Print ("Achtung auf korrekte Rechtschreibung unbedingt achten")
+function = input("Rechenoperation eingeben: Dif, Sum, Multi")
